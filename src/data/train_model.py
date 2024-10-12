3d367349195f5e0f50ae327a9b60bc083c659efb
Fadhil Reza Maulana <fadhilrezam@gmail.com>
2024-10-12 15:27:44 +0700

    'train_model_script_update'

--- src/data/train_model.py
+++ src/data/train_model.py
@@ -3,6 +3,7 @@ import pandas as pd
 from prophet import Prophet
 import mlflow
 import mlflow.prophet
+from mlflow.models.signature import infer_signature
 from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error
 
 
@@ -22,15 +23,15 @@ def train_model(df_train_scaled, df_test_scaled):
         future['y_gspc'] = df_test_scaled['y_gspc']
         prophet_model.fit(df_train_scaled)
         df_predicted = prophet_model.predict(future)[['ds','yhat']]
+        signature = infer_signature(df_test_scaled, df_predicted)
         rmse = root_mean_squared_error(df_predicted['yhat'], df_test_scaled['y'])
-
-        with mlflow.start_run() as run:
-            mlflow.prophet.log_model(prophet_model, 'prophet_model')
-        return rmse
+        return prophet_model, signature, rmse
     except Exception as e:
         print(e)
 
 if __name__ == '__main__':
     df_train_scaled, df_test_scaled = load_preprocessed_dataset()
-    rmse_score = train_model(df_train_scaled, df_test_scaled)
+    prophet_model,signature, rmse_score = train_model(df_train_scaled, df_test_scaled)
+    with mlflow.start_run() as run:
+        mlflow.prophet.log_model(prophet_model, 'model',signature = signature)
     print('RMSE Score:', rmse_score)
